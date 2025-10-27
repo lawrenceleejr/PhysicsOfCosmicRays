c68d2a82a10719515489b6ae395b3bb5e19dff22
Micah Hillman <rhillman@vols.utk.edu>
2025-10-27 12:25:41 -0400

    midi out on coincidence. ditched hardware interrupts.

--- CoincidenceManager/unflash-uno-r3-midi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-sudo dfu-programmer atmega16u2 read
-sudo dfu-programmer atmega16u2 erase
-sudo dfu-programmer atmega16u2 flash /Users/rhillman/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/firmwares/atmegaxxu2/arduino-usbserial/Arduino-usbserial-atmega16u2-Uno-Rev3.hex
-sudo dfu-programmer atmega16u2 reset
